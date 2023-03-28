c358bf340fd88adf1b6db582885c20aa2ffab9e4
umnomequalqu <csa16-6436@colegiosantoantonio.com.br>
2023-03-28 15:33:24 -0300

    atualizando o provider

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -7,8 +7,25 @@ const defaultCartState={
 }
 const cartReducer = (state,action)=>{
     if( action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        const existingcartItemIndex = state.items.findIndex(
+            (item)=> item.id === action.item.id
+        );
+        const existingCartItem= state.items[existingcartItemIndex];
+        let updatedItem;
+        let updatedItems;
+
+        if (existingCartItem){
+            updatedItem={
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items];
+            updatedItems[existingcartItemIndex] = updatedItem;
+        }else{
+            updatedItems = state.items.concat(action.item)
+        }
+        
         return {
             items: updatedItems,
             totalAmount:updatedTotalAmount
